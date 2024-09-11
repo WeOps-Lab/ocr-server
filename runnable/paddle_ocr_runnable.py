0082f4fa348946eb7c2a27f7b946e47e88aa796b
umaru <15875339926@139.com>
2024-09-11 19:22:18 +0800

    refactor: refactor OCR initialization and processing logic
    - Replace `PPStructure` import with `PaddleOCR`
    - Simplify initialization of OCR engine with `PaddleOCR`
    - Update OCR processing call to `self.ocr_engine.ocr(img, cls=True)`
    - Correct loop logic for constructing recognized texts
    
    Signed-off-by: umaru <15875339926@139.com>

--- runnable/paddle_ocr_runnable.py
+++ runnable/paddle_ocr_runnable.py
@@ -6,30 +6,26 @@ import numpy as np
 from langchain_core.documents import Document
 from langchain_core.runnables import RunnableLambda
 from loguru import logger
-from paddleocr import PPStructure
+from paddleocr import PaddleOCR
 
 from user_types.paddle_ocr_request import PaddleOcrRequest
 
 
 class PaddleOcrRunnable:
     def __init__(self):
-        self.ocr_engine = PPStructure(table=True, ocr=True, show_log=True, lang='ch',
-                                      det_model_dir='./models/ch_PP-OCRv4_det_infer/',
-                                      rec_model_dir='./models/ch_PP-OCRv4_rec_infer/',
-                                      cls_model_dir='./models/ch_ppocr_mobile_v2.0_cls_infer/',
-                                      use_angle_cls=True, )
+        self.ocr_engine = PaddleOCR(table=True, show_log=True, lang='ch', use_angle_cls=True, )
 
     def execute(self, request: PaddleOcrRequest) -> List[Document]:
         # base64 str to opencv img
         base_image = base64.b64decode(request.file)
         nparr = np.frombuffer(base_image, np.uint8)
         img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-        result = self.ocr_engine(img)
+        result = self.ocr_engine.ocr(img, cls=True)
         try:
             recognized_texts = ''
-            for line in result:
-                for i in line['res']:
-                    recognized_texts += i['text'] + ' '
+            for lines in result:
+                for line in lines:
+                    recognized_texts += line[1][0] + ''
 
             return [Document(page_content=recognized_texts)]
         except Exception as e:
